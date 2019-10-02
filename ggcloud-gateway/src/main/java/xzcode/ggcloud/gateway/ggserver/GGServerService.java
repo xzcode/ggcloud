d8cfbeb401040dfc2f60b3bc846d92c145fb4545
xiezai <379551875@qq.com>
2019-10-03 00:52:24 +0800

    xx

--- ggcloud-gateway/src/main/java/xzcode/ggcloud/gateway/ggserver/GGServerService.java
+++ ggcloud-gateway/src/main/java/xzcode/ggcloud/gateway/ggserver/GGServerService.java
@@ -1,5 +1,7 @@
 package xzcode.ggcloud.gateway.ggserver;
 
+import xzcode.ggcloud.gateway.ggserver.codec.GatewayDecodeHandler;
+import xzcode.ggcloud.gateway.ggserver.codec.GatewayEncodeHandler;
 import xzcode.ggserver.core.GGServer;
 import xzcode.ggserver.core.config.GGConfig;
 
@@ -8,8 +10,10 @@ public class GGServerService {
 	private GGServer ggServer;
 	
 	private void init() {
-		GGConfig ggServerConfig = new GGConfig();
-		ggServer = new GGServer(ggServerConfig);
+		GGConfig config = new GGConfig();
+		config.setDecodeHandler(new GatewayDecodeHandler());
+		config.setEncodeHandler(new GatewayEncodeHandler());
+		ggServer = new GGServer(config);
 		
 
 	}
